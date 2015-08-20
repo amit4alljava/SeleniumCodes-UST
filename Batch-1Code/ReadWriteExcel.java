9af784c07f938f5523e16ebc41450182c18d9435
amit.srivastava4all@gmail.com <amit.srivastava4all@gmail.com>
2015-08-20 13:05:27 +0530

    Excel Data

---
+++ Batch-1Code/ReadWriteExcel.java
@@ -0,0 +1,56 @@
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+
+public class ReadWriteExcel {
+	
+	public static void main(String[] args) throws Exception {
+		readExcel();
+	}
+	
+	public static void readExcel() throws Exception{
+		int row = 4;
+		int col = 2;
+		int colStart = 0;
+		int rowStart = 0;
+				
+		Object [][] userArray= new Object[row][col]; 
+		// This is for Opening an Excel File
+		FileInputStream file = 
+				new FileInputStream("D:\\Selenium-WS\\xls-file\\UserData.xls");
+		HSSFWorkbook workbook = new HSSFWorkbook(file);
+		HSSFSheet  sheet = workbook.getSheetAt(0);
+		Iterator<Row> rows = sheet.iterator();
+		while(rows.hasNext()){
+			Row currentRow = rows.next();
+			Iterator<Cell> cells = currentRow.cellIterator();
+			colStart = 0;
+			while(cells.hasNext()){
+				Cell currentCell = cells.next();
+				if(currentCell.getCellType()==Cell.CELL_TYPE_STRING){
+					userArray[rowStart][colStart] = currentCell.getStringCellValue();
+				}
+				else
+				if(currentCell.getCellType()==Cell.CELL_TYPE_NUMERIC){
+					userArray[rowStart][colStart] = currentCell.getNumericCellValue();
+				}
+				colStart++;
+			}
+			rowStart++;
+		}
+		
+		System.out.println("*******************************************");
+		for(int i = 0 ; i<row; i++){
+			for(int j= 0 ; j<col; j++ ){
+				System.out.print(userArray[i][j]+" \t");
+			}
+			System.out.println();
+		}
+	}
+
+}
