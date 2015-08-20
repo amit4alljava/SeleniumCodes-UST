3b4ade320d401208d1114690c5e386760b0d3aa1
amit.srivastava4all@gmail.com <amit.srivastava4all@gmail.com>
2015-08-20 16:19:35 +0530

    Data Driven Framework Demo

---
+++ DataDrivenFramework/LoginTest.java
@@ -0,0 +1,76 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+	
+	@DataProvider(name="excelData")
+	public Object[][] excelDataProvider(){
+		Object array [][]=null;
+		
+			try {
+				array = ReadWriteExcel.readExcel();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		return array;
+	}
+	
+	
+	@Test(dataProvider="excelData")
+	public void testLogin(String userData, String pwdData) throws Exception{
+		if(userData!=null && userData.trim().length()>0 )
+		{
+		if(pwdData!=null && pwdData.trim().length()>0){	
+		
+		System.setProperty("webdriver.chrome.driver", 
+				"D:\\Selenium-WS\\Jars\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("http://newtours.demoaut.com/");
+		WebElement userName = driver.findElement(By.name("userName"));
+		userName.clear();
+		//userName.sendKeys("amitsrivastava");
+		userName.sendKeys(userData);
+		Thread.sleep(1000);
+		
+		/*WebElement pwd = driver.findElement(By.name("password"));
+		pwd.clear();
+		pwd.sendKeys("amit123456");*/
+		
+		//driver.findElement(By.name("password")).sendKeys("amit123456");
+		driver.findElement(By.name("password")).sendKeys(pwdData);
+		
+		
+		Thread.sleep(1000);
+		
+		WebElement loginButton = driver.findElement(By.name("login"));
+		loginButton.click();
+		
+		Thread.sleep(1000);
+		
+		
+		List<WebElement> radioList = driver.findElements(By.name("tripType"));
+		radioList.get(1).click();
+		
+		Thread.sleep(2000);
+		
+		WebElement select = driver.findElement(By.name("fromPort"));
+		Select selectFromPort = new Select(select);
+		selectFromPort.selectByVisibleText("Paris");
+		Thread.sleep(2000);
+		driver.close();
+		}
+		}
+		
+	}
+
+}
